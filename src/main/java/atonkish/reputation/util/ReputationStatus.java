8bbb892d14f9d6988a101cf8ddcefa8fa4f189de
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-02-11 13:24:32 +0900

    feat: add reputation enum

---
+++ src/main/java/atonkish/reputation/util/ReputationStatus.java
@@ -0,0 +1,48 @@
+package atonkish.reputation.util;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.util.Formatting;
+
+import atonkish.reputation.ReputationMod;
+
+public enum ReputationStatus {
+    FRIENDLY("friendly", Formatting.DARK_GREEN),
+    TRUSTWORTHY("trustworthy", Formatting.GREEN),
+    NEUTRAL("neutral", Formatting.GRAY),
+    SUSPICIOUS("suspicious", Formatting.RED),
+    HOSTILE("hostile", Formatting.DARK_RED),
+    UNKNOWN("unknown", Formatting.DARK_GRAY);
+
+    private final String translateKey;
+    private final Formatting formatting;
+
+    private ReputationStatus(String status, Formatting formatting) {
+        this.translateKey = "entity." + ReputationMod.MOD_ID + ".villager.reputation." + status;
+        this.formatting = formatting;
+    }
+
+    public static ReputationStatus getStatus(@Nullable Integer reputation) {
+        if (reputation == null) {
+            return UNKNOWN;
+        } else if (reputation <= -100) {
+            return HOSTILE;
+        } else if (reputation < 0) {
+            return SUSPICIOUS;
+        } else if (reputation == 0) {
+            return NEUTRAL;
+        } else if (reputation < 100) {
+            return TRUSTWORTHY;
+        } else {
+            return FRIENDLY;
+        }
+    }
+
+    public String getTranslateKey() {
+        return this.translateKey;
+    }
+
+    public Formatting getFormatting() {
+        return this.formatting;
+    }
+}
